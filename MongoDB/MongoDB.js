245e148ea3d18485ffff571d6f975503ee4b09b3
Omriyonatani <omriyonatani@gmail.com>
2022-04-20 00:02:00 +0300

    finish

---
+++ MongoDB/MongoDB.js
@@ -0,0 +1,52 @@
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri ="mongodb+srv://raafat:eRcywty8yYOG6s9q@cluster0.phfg8.mongodb.net/CallCenter?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const { Parser } = require('json2csv');
+var fs = require('fs');
+
+
+async function insertToMongoDB(data){
+  // Connect to our MongoDB's collection
+  client.connect(err => {
+    if (err) throw err;
+    const collection = client.db("CallCenter").collection("Customers");
+  });
+  // Connect + Insert to the MongoDB
+  client.connect(async function(err, db) {
+    data = JSON.parse(data)
+    if (err) throw err;
+    var dbo = db.db("CallCenter");
+    dbo.collection("Customers").insertOne(data, function(err, res) {
+      if (err) throw err;
+    }); 
+  });
+}
+
+// Function that exports the data to CSV (for BigML)
+function exportToCsv(){
+  client.connect(function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("CallCenter");
+    var query = { };
+    dbo.collection("Customers").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      var fields = ['phoneNumber', 'id', 'city', 'gender', 'age', 'prevCalls','topic','Product','totalTime','totalCalls','season'];
+      const opts = { fields };
+      try {
+        const parser = new Parser(opts);
+        const csv = parser.parse(result);
+        var path='../bigML/MongoData.csv';
+        fs.writeFile(path, csv, function(err,data) {
+            if (err) {throw err;}
+            
+        });
+      } catch (err) {
+        console.error(err);
+      }
+      db.close();
+    });
+  });
+}
+exportToCsv();
+module.exports.insertToMongoDB = insertToMongoDB;
+module.exports.exportToCsv = exportToCsv;
---
+++ kafka/PublishToKafka/publish.js
@@ -0,0 +1,35 @@
+// https://www.cloudkarafka.com/ הפעלת קפקא במסגרת ספק זה
+
+const uuid = require("uuid");
+const Kafka = require("node-rdkafka");
+
+
+
+// From data entry to kafka
+const kafkaConf = {
+  "group.id": "kafka",
+  "metadata.broker.list": "tricycle-01.srvs.cloudkafka.com:9094,tricycle-02.srvs.cloudkafka.com:9094,tricycle-03.srvs.cloudkafka.com:9094".split(","),
+  "socket.keepalive.enable": true,
+  "security.protocol": "SASL_SSL",
+  "sasl.mechanisms": "SCRAM-SHA-256",
+  "sasl.username": "w63twr24",
+  "sasl.password": "aFBdS6zxflHeaCif4m8rnF4GFjhhc6Zp",
+  "debug": "generic,broker,security"
+};
+
+const prefix = "w63twr24-";
+const topic = `${prefix}new`;
+const producer = new Kafka.Producer(kafkaConf);
+
+const genMessage = m => new Buffer.alloc(m.length,m);
+
+producer.on("ready", function(arg) {
+  console.log(`producer ${arg.name} ready.`); 
+});
+
+producer.connect();
+
+module.exports.publish= function(msg){   
+  m=JSON.stringify(msg);
+  producer.produce(topic, -1, genMessage(m), uuid.v4());     
+}
