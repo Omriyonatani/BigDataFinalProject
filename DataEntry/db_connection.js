30df75585863a2816cbc04ba32fc2f23cc7cebad
RaafatMarzuq <57416873+RaafatMarzuq@users.noreply.github.com>
2022-04-22 00:14:13 +0300

    try git

--- DataEntry/db_connection.js
+++ DataEntry/db_connection.js
@@ -9,7 +9,7 @@ function connect(){
   var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "13121994",
+    password: "R0546379423m",
     database: 'bigdatadb'
   });
   return con;
